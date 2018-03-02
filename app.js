7c96d9e20561297ac1eac5681c9ea4716297b736
MaeganW <maeganwomble@gmail.com>
2018-03-01 21:40:34 -0600

    requiring other files

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ console.log('running app.js');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes.js')
 
 const user = os.userInfo();
 console.log(user);
---
+++ notes.js
@@ -0,0 +1,3 @@
+console.log('running notes.js');
+
+console.log(module);
