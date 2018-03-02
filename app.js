8efbf884dc46f0be3ba4a08c3e61cf121ea50704
MaeganW <maeganwomble@gmail.com>
2018-03-01 22:16:10 -0600

    process args

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const fs = require('fs');
 const os = require('os');
 const notes = require('./notes.js')
 
-const user = os.userInfo();
-console.log(user);
-console.log(module);
+// const user = os.userInfo();
+// console.log(user);
+// console.log(module);
+// console.log(notes.sum(12,21))
 
-fs.appendFile('hello.txt', `Hello ${user.username}! You are ${notes.age}. ${notes.addNote()}`);
+// fs.appendFile('hello.txt', `Hello ${user.username}! You are ${notes.age}. ${notes.addNote()}`);
--- notes.js
+++ notes.js
@@ -5,3 +5,7 @@ module.exports.addNote = () => {
   console.log('adding note');
   return 'Here is a new note!'
 }
+module.exports.sum = (a,b) => a + b;
+
+//variables typed into the console
+console.log(process.argv);
