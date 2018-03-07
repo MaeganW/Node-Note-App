cc239576854368627e4347750b79b153ec1ebfc6
MaeganW <maeganwomble@gmail.com>
2018-03-07 09:45:14 -0600

    wip

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ console.log('running app.js');
 
 const fs = require('fs');
 const os = require('os');
-const notes = require('./notes.js')
+const notes = require('./notes.js');
+const yargs = require('yargs');
 
 // const user = os.userInfo();
 // console.log(user);
@@ -10,3 +11,23 @@ const notes = require('./notes.js')
 // console.log(notes.sum(12,21))
 
 // fs.appendFile('hello.txt', `Hello ${user.username}! You are ${notes.age}. ${notes.addNote()}`);
+
+//variables typed into the console
+var command = process.argv[2];
+// console.log(process.argv);
+// console.log('process argv: ', command);
+
+let msg = '';
+if (command === 'add') {
+  msg = 'Adding a note';
+} else if (command === 'delete') {
+  msg = 'Deleting a note';
+} else if (command === 'read') {
+  msg = 'Reading a note';
+} else if (command === 'list') {
+  msg = 'Listing all notes';
+} else {
+  msg = 'Command not recognized';
+}
+
+console.log('Note Taker Says: ', msg);
--- notes.js
+++ notes.js
@@ -6,19 +6,3 @@ module.exports.addNote = () => {
   return 'Here is a new note!'
 }
 module.exports.sum = (a,b) => a + b;
-
-//variables typed into the console
-var command = process.argv[2];
-// console.log(process.argv);
-// console.log('process argv: ', command);
-
-let msg = '';
-if (command === 'add') {
-  msg = 'Adding a note';
-} else if (command === 'delete') {
-  msg = 'Deleting a note';
-} else {
-  msg = 'Command not recognized';
-}
-
-console.log('Note Taker Says: ', msg);
